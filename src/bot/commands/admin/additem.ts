71b994d08c2d9884fbbc0fa518e3966974148605
Zetax <ernzerhofffinn@gmail.com>
2023-07-12 21:23:33 +0200

    Styles now work with /addcosmetic

--- src/bot/commands/admin/additem.ts
+++ src/bot/commands/admin/additem.ts
@@ -2,15 +2,13 @@ export { }
 
 import { CommandInteraction, EmbedBuilder, PermissionFlagsBits } from "discord.js";
 import Asteria from "asteriasdk";
+import error from "../../../utilities/structs/error";
 
 const { SlashCommandBuilder } = require('discord.js');
 const Users = require('../../../model/user');
 const Profiles = require('../../../model/profiles');
-
-const asteria = new Asteria({
-    collectAnonStats: true,
-    throwErrors: true,
-});
+import path from 'path';
+import fs from 'fs';
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -42,65 +40,70 @@ module.exports = {
 
         const cosmeticname: string = interaction.options.getString('cosmeticname');
 
-        const cosmeticCheck = await asteria.getCosmetic("name", cosmeticname, false);
-
-        const regex = /^(?:[A-Z][a-z]*\b\s*)+$/;
-
-        if (!regex.test(cosmeticname)) return await interaction.editReply({ content: "Please check for correct casing. E.g 'renegade raider' is wrong, but 'Renegade Raider' is correct.", ephemeral: true })
-
-        let cosmetic: any = {};
-
         try {
-            cosmetic = await asteria.getCosmetic("name", cosmeticname, false);
-        } catch (err) {
-            return await interaction.editReply({ content: "That cosmetic does not exist" });
-        } finally {
-            try {
-                if (profile.profiles.athena.items[`${cosmeticCheck.type.backendValue}:${cosmeticCheck.id}`]) return await interaction.editReply({ content: "That user already has that cosmetic", ephemeral: true });
-            } catch (err) {
-                await fetch(`https://fortnite-api.com/v2/cosmetics/br/search?name=${cosmeticname}`).then(res => res.json()).then(async json => {
-                    const cosmeticFromAPI = json.data;
-                    if (profile.profiles.athena.items[`${cosmeticFromAPI.type.backendValue}:${cosmeticFromAPI.id}`]) {
-                        await interaction.editReply({ content: "That user already has that cosmetic", ephemeral: true });
-                        return;
+            await fetch(`https://fortnite-api.com/v2/cosmetics/br/search?name=${cosmeticname}`).then(res => res.json()).then(async json => {
+                const cosmeticFromAPI = json.data;
+                if (!cosmeticFromAPI) return await interaction.editReply({ content: "Could not find the cosmetic", ephemeral: true });
+
+                const cosmeticimage = cosmeticFromAPI.images.icon;
+
+                const regex = /^(?:[A-Z][a-z]*\b\s*)+$/;
+                if (!regex.test(cosmeticname)) return await interaction.editReply({ content: "Please check for correct casing. E.g 'renegade raider' is wrong, but 'Renegade Raider' is correct.", ephemeral: true })
+
+                let cosmetic: any = {};
+
+                const file = fs.readFileSync(path.join(__dirname, "../../../../Config/DefaultProfiles/allathena.json"));
+                const jsonFile = JSON.parse(file.toString());
+                const items = jsonFile.items;
+
+                let foundcosmeticname: string = "";
+                let found = false;
+
+                for (const key of Object.keys(items)) {
+                    const [type, id] = key.split(":");
+                    if (id === cosmeticFromAPI.id) {
+                        foundcosmeticname = key;
+                        console.log(`Found key: ${key}`);
+                        if (profile.profiles.athena.items[key]) {
+                            return await interaction.editReply({ content: "That user already has that cosmetic", ephemeral: true });
+                        }
+                        found = true;
+                        cosmetic = items[key];
+                        break;
                     }
-                    cosmetic = cosmeticFromAPI;
-                })
-            }
-        }
+                }
 
-        await Profiles.findOneAndUpdate(
-            { accountId: user.accountId },
-            {
-                $set: {
-                    [`profiles.athena.items.${cosmetic.type.backendValue}:${cosmetic.id}`]: {
-                        templateId: `${cosmetic.type.backendValue}:${cosmetic.id}`,
-                        attributes: {
-                            item_seen: false,
-                            variants: [],
-                            favorite: false,
+                if (!found) return await interaction.editReply({ content: `Could not find the cosmetic ${cosmeticname}`, ephemeral: true });
+
+                await Profiles.findOneAndUpdate(
+                    { accountId: user.accountId },
+                    {
+                        $set: {
+                            [`profiles.athena.items.${foundcosmeticname}`]: cosmetic,
                         },
-                        "quantity": 1,
                     },
-                },
-            },
-            { new: true },
-        )
-            .catch((err) => {
-            })
+                    { new: true },
+                ).catch(async (err) => {
+                    console.log(err);
+                    return await interaction.editReply({ content: "An error occured while adding the cosmetic", ephemeral: true });
+                })
 
-        const embed = new EmbedBuilder()
-            .setTitle("Cosmetic added")
-            .setDescription("Successfully gave the user the cosmetic: " + cosmetic.name)
-            .setThumbnail(cosmetic.images.icon)
-            .setColor("#2b2d31")
-            .setFooter({
-                text: "Momentum",
-                iconURL: "https://cdn.discordapp.com/app-assets/432980957394370572/1084188429077725287.png",
-            })
-            .setTimestamp();
+                const embed = new EmbedBuilder()
+                    .setTitle("Cosmetic added")
+                    .setDescription("Successfully gave the user the cosmetic: " + cosmeticname)
+                    .setThumbnail(cosmeticimage)
+                    .setColor("#2b2d31")
+                    .setFooter({
+                        text: "Momentum",
+                        iconURL: "https://cdn.discordapp.com/app-assets/432980957394370572/1084188429077725287.png",
+                    })
+                    .setTimestamp();
 
-        await interaction.editReply({ embeds: [embed] });
+                await interaction.editReply({ embeds: [embed] });
+            })
+        } catch (err) {
+            console.log(err);
+        }
 
     },
 };
